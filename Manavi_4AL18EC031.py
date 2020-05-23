b623b9a22a541628b97916eed7611f77a70639c5
Manavii <65395747+Manavii@users.noreply.github.com>
2020-05-23 17:45:31 +0530

    Create Manavi_4AL18EC031.py

---
+++ Manavi_4AL18EC031.py
@@ -0,0 +1,11 @@
+username_value = "Micheal"
+input_username = ""
+password_value = "e3$WT89x"
+input_password = ""
+
+while input_username != username_value:
+input_username = input("Enter Username: ")
+while input_password != password_value:
+input_password = input("Enter Password: ")
+
+print("you are successfully logged in!")
