175227d3991ba74ca829d88ed5b6b1dfeabd9129
Parveez Shariff <parveez1shariff@gmail.com>
2020-05-24 15:04:49 +0530

    Verified
    Rectify the code

--- Manavi_4AL18EC031.py
+++ Manavi_4AL18EC031.py
@@ -9,3 +9,12 @@ while input_password != password_value:
 input_password = input("Enter Password: ")
 
 print("you are successfully logged in!")
+
+# Your code has indenting error, secondly the output is not correct
+Enter Username: jac
+
+Enter Username: jack
+jack
+Enter Username: w3
+w3
+Enter Username: 
