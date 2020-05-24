812912d12e46f88da63726ddd31049e3dbc39898
vaish54 <65485788+vaish54@users.noreply.github.com>
2020-05-24 17:35:37 +0530

    Add files via upload

---
+++ vaishnavi_4AL18EC055.py
@@ -0,0 +1,10 @@
+username=input("enter username")
+key=input("enter password")
+count=0
+user="Micheal"
+password="e3$WT89x"
+while count<3:
+ if key==password and username==user:
+  print("login successful")
+ else:
+  print("invalid credentials")
