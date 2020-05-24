cf4984d7115be6e325cd2b2193aee99b0548f79a
Parveez Shariff <parveez1shariff@gmail.com>
2020-05-24 15:07:54 +0530

    Verfied
    Fix the bug

--- Neha T_4AL183C035.py
+++ Neha T_4AL183C035.py
@@ -1,4 +1,4 @@
-attemps=0
+attempts=0
 while attempts<3:
    username=input('Enter your username')
    password=input('Enter your password')
@@ -9,3 +9,8 @@ while attempts<3:
         print('Invalid username and password')
         if attempts==3:
            print ('account locked')
+# Your code is able to verify correctly but even after successful login again it is asking to provide the details, so rectify the bug
+Enter your usernameMicheal
+Enter your passworde3$WT89x
+You are successfully logged in
+Enter your username
