6006bdda5d319a3280574daa50dfbc2f2f35f86c
psraipreetham <65494951+psraipreetham@users.noreply.github.com>
2020-06-01 13:04:17 +0530

    Add files via upload

---
+++ PREETHAMSRAI (1).py
@@ -0,0 +1,12 @@
+﻿total_attempt=0
+while total_attempt<3:
+           username=input(“enter your user name: ”)
+           password=input(“enter your password ”)
+           If username==’Michal' and password==’e3$WT89x':
+                  print(“you are successfully loged in")
+          else:        
+                 print(“you entered wrong login id. Please check again\n”)  
+                 print(‘you have’ ,3-total_attempt)   
+                total_attempt+=1
+                if total_attempt==3:
+                   print(“account blocked")
